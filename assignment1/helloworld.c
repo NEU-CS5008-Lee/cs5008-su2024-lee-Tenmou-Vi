b6f02b6c9e1960b503cff52c1aa216fc5b711bd9
Tenmou-Vi <tenmousha@gmail.coml>
2024-05-23 15:29:36 -0400

    upload assignment 1

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// name: Jeongkyu Lee 
-// email: jeo.lee@northeastern.edu
+// name: Tianmeng Xia 
+// email: xia.tianm@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is [YourName].\n");
-  printf("I am assignment 1 for [This Semester].\n");
+  printf("Hello World.  This is Tianmeng Xia.\n");
+  printf("I am assignment 1 for 2024 summer.\n");
   return 0;
 }
