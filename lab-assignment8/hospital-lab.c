c316ac1f993cd5977a382b251b5091fd74f7f4a6
Tenmou-Vi <tenmousha@gmail.coml>
2024-07-04 19:08:48 -0400

    lab8

--- lab-assignment8/hospital-lab.c
+++ lab-assignment8/hospital-lab.c
@@ -1,6 +1,6 @@
 /* Lab Assignment for Hospital ER */
-//enter your name here
-//enter your email here
+//Xia Tianmeng
+//tenmousha@gmail.com
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -49,6 +49,16 @@ void heapify(pq*p2,int n, int i ){
     int lchild=2*i+1;/*left =2*i+1*/
     int rchild=2*i+2;/*right=2*i+2*/
     /*insert your code here*/
+    if (lchild < n && p2->heap[lchild].priority > p2->heap[largest].priority)
+        largest = lchild;
+
+    if (rchild < n && p2->heap[rchild].priority > p2->heap[largest].priority)
+        largest = rchild;
+
+    if (largest != i) {
+        swap(&p2->heap[i], &p2->heap[largest]);
+        heapify(p2, n, largest);
+    }
 
 }
 
@@ -56,7 +66,11 @@ void heapify(pq*p2,int n, int i ){
 void shiftUp(pq* p2,int i)
 {
     /*insert your code here*/
-       
+        int parent = (i - 1) / 2;
+    if (i && p2->heap[parent].priority < p2->heap[i].priority) {
+        swap(&p2->heap[i], &p2->heap[parent]);
+        shiftUp(p2, parent);
+    }	
     
 }
  
