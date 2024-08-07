368b61be54b089d9c59e23143755f2ef303ef400
Tenmou-Vi <tenmousha@gmail.coml>
2024-08-06 23:12:17 -0400

    lab13

--- lab-assignment13/mp3-lab.c
+++ lab-assignment13/mp3-lab.c
@@ -1,20 +1,25 @@
-//enter your email here
-//enter your name here
+//tenmousha@gmail.com
+//Tianmeng Xia
 
 #include<stdio.h>
 #include<string.h>
 
 int compression(char arr[], int n, char res[]){
-    
-    
-    
-    
-    //insert your code here
-    
-    
-    
-    
-    
+    int res_index = 0;
+    for(int i = 0; i < n; i++) {
+        int count = 1;
+        while(i < n - 1 && arr[i] == arr[i + 1]) {
+            count++;
+            i++;
+        }
+        res[res_index++] = arr[i];
+        if(count > 1) {
+            int len = sprintf(&res[res_index], "%d", count);
+            res_index += len;
+        }
+    }
+    res[res_index] = '\0';
+    return res_index;
 }
  
 int main()
